d8a11037db5228228fe8b8900be48856289e1133
Mariana Maldonado <mmaldonado9@ucol.mx>
2023-06-27 10:13:46 -0600

    Add user component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useForm } from 'react-hook-form'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const AddUserForm = (props) => {
+
+    const {register, errors, handleSubmit} = useForm();
+
+    const onSubmit = (data, e) => {
+        data.id = null
+        console.log(data)
+        props.addUser(data)
+        e.target.reset();
+    }
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <label>Name</label>
+            <input 
+                type="text" 
+                name="name"
+                ref={register({required: {value: true, message: 'Valor requerido'}})}
+                />
+            <div>
+                {errors?.name?.message}
+            </div>
+            <label>Username</label>
+            <input 
+                type="text" 
+                name="username" 
+                ref={register({required: {value: true, message: 'Valor requerido'}})}
+                />
+            <div>
+                {errors?.username?.message}
+            </div>
+            <button type="submit">Add new user</button>
+        </form>
+    );
 }
-
-export default App;
+ 
+export default AddUserForm;

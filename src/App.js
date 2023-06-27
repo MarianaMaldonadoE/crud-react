c35f1f50c06170c9bba270b2b316a98df75b1bb9
Mariana Maldonado <mmaldonado9@ucol.mx>
2023-06-27 10:16:11 -0600

    Add project components

--- src/App.js
+++ src/App.js
@@ -1,40 +1,78 @@
-import React from 'react';
-import { useForm } from 'react-hook-form'
+import React, {useState} from 'react';
+import UserTable from './components/UserTable';
+import AddUserForm from './components/AddUserForm';
+import EditYo from './components/EditYo';
+import { v4 as uuidv4 } from 'uuid';
 
-const AddUserForm = (props) => {
+function App() {
 
-    const {register, errors, handleSubmit} = useForm();
+  // Agregar usuarios
+  const usersData = [
+    { id: uuidv4(), name: 'Tania', username: 'floppydiskette' },
+    { id: uuidv4(), name: 'Craig', username: 'siliconeidolon' },
+    { id: uuidv4(), name: 'Ben', username: 'benisphere' },
+  ]
 
-    const onSubmit = (data, e) => {
-        data.id = null
-        console.log(data)
-        props.addUser(data)
-        e.target.reset();
-    }
+  const [users, setUsers] = useState(usersData)
 
-    return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <label>Name</label>
-            <input 
-                type="text" 
-                name="name"
-                ref={register({required: {value: true, message: 'Valor requerido'}})}
-                />
-            <div>
-                {errors?.name?.message}
-            </div>
-            <label>Username</label>
-            <input 
-                type="text" 
-                name="username" 
-                ref={register({required: {value: true, message: 'Valor requerido'}})}
-                />
-            <div>
-                {errors?.username?.message}
-            </div>
-            <button type="submit">Add new user</button>
-        </form>
-    );
+  const addUser = (user) => {
+    user.id = uuidv4()
+    console.log(user)
+    setUsers([
+      ...users,
+      user
+    ])
+  }
+
+  // Eliminar usuario
+  const deleteUser = id => {
+    setUsers(users.filter(user => user.id !== id))
+  }
+
+  // Editar usuario
+  const [editing, setEditing] = useState(false)
+
+  const initialFormState = { id: null, name: '', username: '' }
+  const [currentUser, setCurrentUser] = useState(initialFormState)
+
+  const editRow = user => {
+    setEditing(true) 
+    setCurrentUser({ id: user.id, name: user.name, username: user.username })
+  }
+
+  const updateUser = (id, updatedUser) => {
+    setEditing(false)
+    setUsers(users.map(user => (user.id === id ? updatedUser : user)))
+  }
+
+  return (
+    <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+        {editing ? (
+          <div>
+            <h2>Edit user</h2>
+            <EditYo 
+              setEditing={setEditing}
+              currentUser={currentUser}
+              updateUser={updateUser}
+            />
+          </div>
+        ) : (
+          <div>
+            <h2>Add user</h2>
+            <AddUserForm addUser={addUser}  />
+          </div>
+        )}
+      </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+          <UserTable users={users} deleteUser={deleteUser} editRow={editRow} />
+        </div>
+      </div>
+    </div>
+  );
 }
- 
-export default AddUserForm;
+
+export default App;
